8096ae2696175312f2ce4087e79e6b0ba631a922
Diego Linares <diego.linares@claro.com.co>
2026-02-10 22:08:32 -0500

    Arreglo de modelos

--- models/Mensaje.js
+++ models/Mensaje.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 
+
+const ItemSchema = new mongoose.Schema({
+  producto: { type: String, required: true },
+  cantidad: { type: Number, required: true },
+}, { _id: false });
+
 const mensajeSchema = new mongoose.Schema({
   numero: { type: String, required: true },
   cuerpo: { type: String, required: true },
-  direccion: { type: String, required: true },  // ?? este es el que falta
+  direccion: { type: String, required: true }, 
+  cliente: { type: String },
+  pedido: { type: [ItemSchema], default: [] }, // ?? este es el que falta
   fecha: { type: Date, default: Date.now }
 });
+
+
 
 export default mongoose.model('Mensaje', mensajeSchema);
 
